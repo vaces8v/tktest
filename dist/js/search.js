13f0bbe4cc5e012a3f5b7eed900cf3619e142e75
Vaceslav <vaceslavupcotc@gmail.com>
2024-01-15 07:33:39 +0300

    update1.0

---
+++ dist/js/search.js
@@ -0,0 +1,5 @@
+const searchButton = document.querySelector('.searchbar-button a');
+searchButton.addEventListener('click', function() {
+    const searchQuery = document.querySelector('.input-search input[type="search"]').value;
+    window.location.href = `/libary.html?search=${encodeURIComponent(searchQuery)}`;
+});
